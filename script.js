85beb3500412fc35f4445a8f4c49225c47d5223d
QuantZero <yigitkasapoglu95@gmail.com>
2022-11-08 19:43:23 -0800

    Adding loader and twitter button response

--- script.js
+++ script.js
@@ -3,19 +3,54 @@ const quoteText = document.getElementById(`quote`)
 const authorText = document.getElementById(`author`)
 const twitterBtn = document.getElementById(`twitter`)
 const newQuoteBtn = document.getElementById(`new-quote`)
+const loader = document.getElementById(`loader`)
 
 
 
 // Getting quotes from API
 let apiQuotes = [];
 
+
+// Show Loading
+function loading() {
+    loader.hidden = false; 
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true
+}
 // Show New Quote
 function newQuote() {
+    loading()
+    // Pick a random quote from the apiQUotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-    console.log(quote)
+    // Check if author field is blank and replace with `Unknown`
+    if (!quote.author) {
+        authorText.textContent = `Unknown`;
+    } else {
+        authorText.textContent = quote.author;
+
+    }
+    
+    // Checking quote length to tdetermine style
+    if (quote.text.length >80) {
+        quoteText.classList.add(`long-quote`)
+    } else {
+        quoteText.classList.remove(`long-quote`)
+    }
+
+// Set quote, hide loader
+    quoteText.textContent = quote.text;
+    complete();
+
 };
 
+// Get Quotes
 async function getQuotes() {
+    loading ();
     const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json'
     try {
         const response = await fetch(apiURL);
@@ -28,6 +63,17 @@ async function getQuotes() {
 }
 
 
+// Tweet Quote
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+    window.open(twitterUrl, `_blank`); //Opens twitter page on a new tab
+
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener(`click`, newQuote);
+twitterBtn.addEventListener(`click`, tweetQuote);
 
 // On Load
 getQuotes();
